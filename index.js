0f6f41c23cc6b04f7d84548b74f9267f7feb227d
emelendez <exmelendez@gmail.com>
2019-10-30 15:24:58 -0400

    updates

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('public'));
+
+app.listen(3000, () => console.log('listening at 3000')); //Listening on port 3000
+
+app.use(express.static('public')); //Serving public folder
+app.use(express.json({ limit: '1mb' })); //ability to interpret incoming data as JSON
+
+app.post('/api', (request, response) => {
+    console.log('I got a request!');
+    console.log(request.body);
+
+    const data = request.body;
+
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.long
+    });
+});
